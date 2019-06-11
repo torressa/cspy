cce5b493c31171d886523fa6386dcc32cf1ca4be
kermit-z <d.torressanchez@lancaster.ac.uk>
2019-06-11 16:33:22 +0100

    fixed circleci integration: import error

--- cspy/__init__.py
+++ cspy/__init__.py
@@ -1,9 +1,16 @@
-import algorithms
-from algorithms import BiDirectional
-import preprocessing
-from preprocessing import preprocess_graph
-import label
+import os
+import sys
+cwd = os.getcwd()
+sys.path.insert(0, cwd)
+
+
 from label import Label
+from preprocessing import preprocess_graph
+from algorithms import BiDirectional
+
+# import cspy.algorithms
+# import cspy.preprocessing
+#import cspy.label
 
 name = "cspy"
 __all__ = ['BiDirectional', 'preprocess_graph', 'Label']
