65dd6e7a7968c00ff29f3101e7280566be27c5c0
torressa <23246013+torressa@users.noreply.github.com>
2021-01-18 21:23:31 +0100

    Reorder gtest import in CMake

--- cmake/cpp.cmake
+++ cmake/cpp.cmake
@@ -29,16 +29,6 @@ if(CHECK_TYPE)
 endif()
 
 add_subdirectory(src/cc/)
-if (BUILD_TESTING)
-  include(FetchContent)
-  FetchContent_Declare(
-    googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG master)
-  FetchContent_MakeAvailable(googletest)
-  set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-  add_subdirectory(test/cc/)
-endif()
 
 # Install
 install(EXPORT cspyTargets
