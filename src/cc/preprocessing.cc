41f15c8dc98e382c54a5e384d301559979a616b9
torressa <23246013+torressa@users.noreply.github.com>
2021-05-04 10:47:25 +0100

    Add explicit types for preprocessing, remove lemon check

--- src/cc/preprocessing.cc
+++ src/cc/preprocessing.cc
@@ -10,31 +10,58 @@
 
 namespace bidirectional {
 
+// TODO Use explicit types when calling BF to avoid error on MACOS
+
 void shortest_path(
     std::vector<double>* lower_bound_weight,
     const DiGraph&       graph,
     const bool&          forward) {
   // Create map to store distances
-  LemonGraph::NodeMap<double> distance_map(*graph.lemon_graph_ptr);
-  // ref source + sink
-  const LemonNode& source = graph.getLNodeFromId(graph.source.lemon_id);
-  const LemonNode& sink   = graph.getLNodeFromId(graph.sink.lemon_id);
   if (forward) {
     // Instantiate algorithm with normal graph
-    bellmanFord(*graph.lemon_graph_ptr, *graph.weight_map_ptr)
-        .distMap(distance_map)
-        .run(source, sink);
+    LemonGraph::NodeMap<double> distance_map(*graph.lemon_graph_ptr);
+    const LemonNode& source = graph.getLNodeFromId(graph.source.lemon_id);
+    lemon::BellmanFord<LemonGraph, LemonGraph::ArcMap<double>> BF(
+        *graph.lemon_graph_ptr, *graph.weight_map_ptr);
+    BF.distMap(distance_map);
+    BF.run(source, graph.number_edges);
+    // TODO: Fix wizard version on MACOS
+    // bellmanFord(*graph.lemon_graph_ptr, *graph.weight_map_ptr)
+    //     .distMap(distance_map)
+    //     .run(source, sink);
+
+    // Extract shortest path distance to each node, if available, ow lemon::inf
+    for (LemonGraph::NodeIt v(*graph.lemon_graph_ptr); v != lemon::INVALID;
+         ++v) {
+      const int& id = graph.getId(v);
+      // std::cout << "dist[" << id << "] = " << distance_map[v] << "\n";
+      (*lower_bound_weight)[id] = distance_map[v];
+    }
   } else {
+    lemon::ReverseDigraph<const LemonGraph>::NodeMap<double> distance_map_rev(
+        *graph.lemon_graph_ptr);
+    const LemonNode& sink = graph.getLNodeFromId(graph.sink.lemon_id);
+    lemon::BellmanFord<
+        lemon::ReverseDigraph<const LemonGraph>,
+        LemonGraph::ArcMap<double>>
+        BF(reverseDigraph(*graph.lemon_graph_ptr), *graph.weight_map_ptr);
+    BF.distMap(distance_map_rev);
+    BF.run(sink, graph.number_edges);
+
+    // TODO: Fix wizard version on MACOS
     // Instantiate algorithm with reversed digraph
-    bellmanFord(reverseDigraph(*graph.lemon_graph_ptr), *graph.weight_map_ptr)
-        .distMap(distance_map)
-        .run(sink, source);
-  }
-  // Extract shortest path distance to each node, if available, ow lemon::inf
-  for (LemonGraph::NodeIt v(*graph.lemon_graph_ptr); v != lemon::INVALID; ++v) {
-    const int& id = graph.getId(v);
-    // std::cout << "dist[" << id << "] = " << distance_map[v] << "\n";
-    (*lower_bound_weight)[id] = distance_map[v];
+    // bellmanFord(reverseDigraph(*graph.lemon_graph_ptr),
+    // *graph.weight_map_ptr)
+    //     .distMap(distance_map)
+    //     .run(sink, source);
+
+    // Extract distance using reverse map
+    for (LemonGraph::NodeIt v(*graph.lemon_graph_ptr); v != lemon::INVALID;
+         ++v) {
+      const int& id = graph.getId(v);
+      // std::cout << "dist[" << id << "] = " << distance_map[v] << "\n";
+      (*lower_bound_weight)[id] = distance_map_rev[v];
+    }
   }
 }
 
